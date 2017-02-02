c3fe9fd3ddfa9e1e370b7b43d4d0af507451882a
taskie <t@skie.jp>
2017-02-03 03:42:10 +0900

    symlink from dst/ to $HOME/

--- sync.sh
+++ sync.sh
@@ -2,10 +2,18 @@
 set -eu
 
 NO_PULL=
+LINK=
+FORCE=
 
-while getopts n OPT
+while getopts fln OPT
 do
     case $OPT in
+        f)
+            FORCE=1
+            ;;
+        l)
+            LINK=1
+            ;;
         n)
             NO_PULL=1
             ;;
@@ -21,3 +29,28 @@ if [ -z "$NO_PULL" ]; then
 fi
 
 bin/polkadot ./entry.yml public/ private/ local/
+
+if [ -n "$LINK" ]; then
+    for file in $(find dst)
+    do
+        if [ "$file" == "dst" -o "$file" == "dst/.gitkeep" ]; then
+            continue
+        fi
+        src="$(pwd)/$file"
+        dst="$HOME/${file##dst/}"
+        echo "$src -> $dst"
+        if [ -d "$src" ]; then
+            mkdir -p "$dst"
+        elif [ -f "$src" ]; then
+            if [ -f "$dst" ]; then
+                if [ -n "$FORCE" ]; then
+                    rm -f "$dst"
+                else
+                    rm -i "$dst"
+                fi
+            fi
+            ln -s "$src" "$dst"
+        fi
+
+    done
+fi
