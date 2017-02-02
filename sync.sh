6e74e3d43d90924f78800d654de188ecc2ae0e10
taskie <t@skie.jp>
2017-02-03 04:43:09 +0900

    download bin/polkadot

--- sync.sh
+++ sync.sh
@@ -20,12 +20,35 @@ do
     esac
 done
 
+if [ ! -f bin/polkadot ]; then
+    echo "bin/polkadot not found."
+    echo "please input target (linux-amd64, linux-386, linux-arm, darwin-amd64, windows-amd64)"
+    echo -n "> "
+    read TARGET
+    if [ "$TARGET" == windows-amd64 ]; then
+        TARGET=windows-amd64.exe
+    fi
+    if [ -z "$TARGET" ]; then
+        exit 1
+    fi
+    curl -L -o bin/polkadot "https://github.com/taskie/polkadot/releases/download/v0.0.1/polkadot-0.0.1-$TARGET"
+    chmod u+x bin/polkadot
+fi
+
 if [ -z "$NO_PULL" ]; then
     git pull
     git submodule init
     git submodule sync
     git submodule update
     echo
+fi
+
+if [ -x private.sh ]; then
+    if [ -n "$NO_PULL" ]; then
+        ./private.sh -n
+    else
+        ./private.sh
+    fi
 fi
 
 bin/polkadot ./entry.yml public/ private/ local/
