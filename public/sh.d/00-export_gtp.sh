5fafa50f9172ed9a39f6b77e5bc47eb5845ff87e
taskie <t@skie.jp>
2017-12-22 15:04:03 +0900

    sh: screen-256color -> xterm-256color

--- public/sh.d/00-export_gtp.sh
+++ public/sh.d/00-export_gtp.sh
@@ -3,6 +3,6 @@ export VISUAL="$EDITOR"
 export GIT_EDITOR="$EDITOR"
 export LANG=ja_JP.UTF-8
 export KCODE=u
-export TERM=screen-256color
+export TERM=xterm-256color
 export JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
 export FILTER="{{if .fzf}}fzf-tmux{{else if .peco}}peco{{end}}"
