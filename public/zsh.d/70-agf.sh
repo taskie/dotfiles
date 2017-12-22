41071f04781c149d4ae64814be3e8bd34afebfa3
taskie <t@skie.jp>
2017-12-22 14:32:24 +0900

    zsh: agf

--- public/zsh.d/70-agf.sh
+++ public/zsh.d/70-agf.sh
@@ -1,22 +1,26 @@
 # http://futurismo.biz/archives/2514
 if type ag >/dev/null 2>&1; then
+    FZF=fzf
+    [ -n "$TMUX" ] && FZF=fzf-tmux || :
     function agf () {
-	list=$(ag --color $@ \
-	    | "$FZF" --ansi --query "$LBUFFER" \
-	    | awk -F : '{print "+" $2 " " $1}')
-	if [ $? -eq 0 -a -n "$list" ]; then
-	    echo "$list" | xargs emacsclient -nw
-	fi
+        list=$(ag --color $@ \
+                      | "$FZF" --ansi --query "$LBUFFER" \
+                      | awk -F : '{print "+" $2 " " $1}')
+        if [ $? -eq 0 -a -n "$list" ]; then
+            echo "$list" | xargs emacsclient -nw
+        fi
     }
 fi
 
 if type rg >/dev/null 2>&1; then
+    FZF=fzf
+    [ -n "$TMUX" ] && FZF=fzf-tmux || :
     function rgf () {
-	list=$(rg -n --color ansi $@ \
-	    | "$FZF" --ansi --query "$LBUFFER" \
-	    | awk -F : '{print "+" $2 " " $1}')
-	if [ $? -eq 0 -a -n "$list" ]; then
-	    echo "$list" | xargs emacsclient -nw
-	fi
+        list=$(rg -n --color ansi $@ \
+                      | "$FZF" --ansi --query "$LBUFFER" \
+                      | awk -F : '{print "+" $2 " " $1}')
+        if [ $? -eq 0 -a -n "$list" ]; then
+            echo "$list" | xargs emacsclient -nw
+        fi
     }
 fi
