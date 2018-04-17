e26a81cfee506ace092b6dca61cc9680700cde69
taskie <t@skie.jp>
2018-04-17 12:26:02 +0900

    emacs: typescript-mode

--- public/emacs.d/init.d/50-typescript-mode.el
+++
@@ -1,9 +0,0 @@
-(when (require 'typescript-mode nil t)
-  (add-to-list 'auto-mode-alist '("\\.ts[x]$" . typescript-mode))
-
-  (add-to-list 'exec-path "/usr/local/share/npm/bin")
-  ;; (require 'tss)
-  ;; (setq tss-popup-help-key "C-:")
-  ;; (setq tss-jump-to-definition-key "C->")
-  ;; (tss-config-default)
-  )
