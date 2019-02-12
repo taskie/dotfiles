0f8280c17a0db78f3f2311a66197d9671b4a53a9
taskie <t@skie.jp>
2019-02-13 08:43:20 +0900

    sh: emacs guard

--- public/sh.d/40-emacs_emacsd.sh
+++ public/sh.d/40-emacs_emacsd.sh
@@ -1,4 +1,4 @@
-if emacsclient -e '(version)' >/dev/null 2>&1; then
+if pgrep '[Ee]macs' || emacsclient -e '(version)' >/dev/null 2>&1; then
     : # Emacs server is already running
 else
     `emacs --daemon`
