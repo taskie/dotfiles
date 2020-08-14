073cdc619e4d1a45c9d0a729fc3e3db47c4b2b57
taskie <t@skie.jp>
2020-08-14 10:16:45 +0900

    fzf: --no-sort

--- public/zsh.d/70-filter-select-history.sh
+++ public/zsh.d/70-filter-select-history.sh
@@ -1,5 +1,5 @@
 function filter-select-history() {
-    BUFFER=$(history 1 | sort -k1,1nr | perl -ne 'BEGIN { my @lines = (); } s/^\s*\d+\s*//; $in=$_; if (!(grep {$in eq $_} @lines)) { push(@lines, $in); print $in; }' | $FILTER --query "$LBUFFER")
+    BUFFER=$(history 1 | sort -k1,1nr | perl -ne 'BEGIN { my @lines = (); } s/^\s*\d+\s*//; $in=$_; if (!(grep {$in eq $_} @lines)) { push(@lines, $in); print $in; }' | $FILTER --query "$LBUFFER" --no-sort)
     CURSOR=${#BUFFER}
 }
 zle -N filter-select-history
