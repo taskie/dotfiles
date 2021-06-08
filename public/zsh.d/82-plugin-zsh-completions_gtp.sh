2da4deffe078a00adaa140abe05498ba6ebb055e
taskie <t@skie.jp>
2021-06-08 10:28:18 +0900

    zsh: add compdef for aliases

--- public/zsh.d/82-plugin-zsh-completions_gtp.sh
+++ public/zsh.d/82-plugin-zsh-completions_gtp.sh
@@ -1,3 +1,9 @@
 if [[ -f "{{.dotfiles}}/submodules/zsh-completions/zsh-completions.plugin.zsh" ]]; then
     source "{{.dotfiles}}/submodules/zsh-completions/zsh-completions.plugin.zsh"
 fi
+
+# https://qiita.com/2357gi/items/e4744ccecccf0bfb7137
+compdef b=bat
+compdef g=git
+compdef sctl=systemctl
+compdef uctl=systemctl
