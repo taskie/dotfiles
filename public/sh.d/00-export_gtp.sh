3aa95db88571b185704c06ab58739241c95e7742
taskie <t@skie.jp>
2024-01-17 11:17:28 +0900

    fix: remove TERM settings

--- public/sh.d/00-export_gtp.sh
+++ public/sh.d/00-export_gtp.sh
@@ -5,5 +5,7 @@ export VISUAL="$EDITOR"
 export GIT_EDITOR="$EDITOR"
 export LANG='{{if .lang}}{{.lang}}{{else}}ja_JP.UTF-8{{end}}'
 export KCODE=u
-export TERM='{{if .term}}{{.term}}{{else}}xterm-256color{{end}}'
 export FILTER='{{if .fzf}}fzf-tmux{{end}}'
+{{if .term}}
+export TERM='{{.term}}'
+{{end}}
