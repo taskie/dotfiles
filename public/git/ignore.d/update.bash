00ac8ba8ce5e46276fa21f43344d272d83837b45
taskie <t@skie.jp>
2026-03-14 21:05:38 +0900

    feat(gitignore): update

--- public/git/ignore.d/update.bash
+++ public/git/ignore.d/update.bash
@@ -5,6 +5,9 @@ BASE_URL=https://raw.githubusercontent.com/github/gitignore/main/Global
 
 find . -name '*.gitignore' | xargs -r -I '{}' basename '{}' .gitignore | while read -r name; do
   nomod_name="$(cut -d _ -f 1 <<<"$name")"
+  if [ "$nomod_name" = custom ]; then
+    continue
+  fi
   url="${BASE_URL}/${nomod_name}.gitignore"
   curl -fsSL "$url" -o "${name}.gitignore" || echo "FAILED: ${url}"
 done
