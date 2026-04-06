e99f38a8b79bbae852e774d0f0dd64d41174aa0a
taskie <t@skie.jp>
2026-04-07 00:13:30 +0900

    feat(sh): set LC_ALL

--- public/sh.d/00-export_gtp.sh
+++ public/sh.d/00-export_gtp.sh
@@ -4,6 +4,7 @@ export EDITOR='{{if .emacsd}}emacsclient -nw{{else}}vim{{end}}'
 export VISUAL="$EDITOR"
 export GIT_EDITOR="$EDITOR"
 export LANG='{{if .lang}}{{.lang}}{{else}}ja_JP.UTF-8{{end}}'
+export LC_ALL="$LANG"
 export KCODE=u
 export FILTER='{{if .fzftmux}}{{.fzftmux}}{{else if .fzf}}{{.fzf}}{{end}}'
 {{if .term}}
