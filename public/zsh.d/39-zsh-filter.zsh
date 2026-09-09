67beb1303c9b91f924a4ebb9bb1591349dcffa99
taskie <t@skie.jp>
2026-09-09 23:41:35 +0900

    perf(zsh): make filter-select-history faster

--- public/zsh.d/39-zsh-filter.zsh
+++ public/zsh.d/39-zsh-filter.zsh
@@ -13,21 +13,9 @@ zle -N filter-find-file
 bindkey '^x^f' filter-find-file
 
 function filter-select-history() {
-    BUFFER=$(
-        history 1 \
-            | sort -k1,1nr \
-            | perl -ne '
-                BEGIN { my @lines = (); }
-                s/^\s*\d+\*?\s*//;
-                $in=$_;
-                if (!(grep {$in eq $_} @lines)) {
-                    push(@lines, $in);
-                    print $in;
-                }
-                ' \
-            | $FILTER --query "$LBUFFER" --no-sort
-    )
+    BUFFER=$(fc -lnr 1 | awk '!seen[$0]++' | "$FILTER" --query "$LBUFFER" --no-sort)
     CURSOR=${#BUFFER}
+    zle redisplay
 }
 
 zle -N filter-select-history
