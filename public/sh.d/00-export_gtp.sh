5cd09222647b2b3c37de9cee9c493257a2a2fa9c
taskie <t@skie.jp>
2026-09-14 23:56:38 +0900

    feat(sh): remove unneeded envvars

--- public/sh.d/00-export_gtp.sh
+++ public/sh.d/00-export_gtp.sh
@@ -4,8 +4,6 @@ export EDITOR='{{if .emacsd}}emacsclient -nw{{else}}vim{{end}}'
 export VISUAL="$EDITOR"
 export GIT_EDITOR="$EDITOR"
 export LANG='{{if .lang}}{{.lang}}{{else}}ja_JP.UTF-8{{end}}'
-export LC_ALL="$LANG"
-export KCODE=u
 export FILTER='{{if .fzftmux}}{{.fzftmux}}{{else if .fzf}}{{.fzf}}{{end}}'
 {{if .term}}
 export TERM='{{.term}}'
